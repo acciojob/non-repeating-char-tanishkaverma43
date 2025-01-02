60de5dc5eb78be3aee2dd8fd862afa34be88c581
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-02 17:23:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	  let charCount = {};
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (charCount[str[i]] === 1) {
+      return str[i];
+    }
+  }
+
+  return null;
+
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
